c5ee151ccebd676ba5169485b295c62d5e8a52d6
PatthanaskCH <patthanasak.ch@ku.th>
2024-08-21 14:57:49 +0700

    testMethod

---
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -0,0 +1,14 @@
+package ku.cs.models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentListTest {
+    @Test
+    @DisplayName("ทดสอบ viewGradeOfId")
+    void testViewGradeOfId() {
+        //
+    }
+}
